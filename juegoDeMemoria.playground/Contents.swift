21952754ac996a837b5ffd2a6ac00c96ced5a9bc
Porfirio <lalorapr@gmail.com>
2017-12-07 18:17:50 -0500

    VERSION FINAL

--- juegoDeMemoria.playground/Contents.swift
+++ juegoDeMemoria.playground/Contents.swift
@@ -1,4 +1,12 @@
-//Proyecto Juego de Memoria
+/*Juego de Memoria
+Generar un rango de 0 a 100, incluye el número 100 en el rango.
+Tienes que iterar el rango completo, utilizando la sentencia for para obtener cada número del rango y aplicar las siguientes reglas de impresión.
+Al evaluar cada número debes aplicar las siguientes reglas:
+- Si el número es divisible entre 5, imprime: # el número  + “Bingo!!!”
+- Si el número es par, imprime: # el número + “par!!!”
+- Si el número es impar, imprime: # el número + “impar!!!”
+- Si el número se encuentra dentro de un rango del 30 al 40, imprime: # el número +  “Viva Swift!!!”
+*/
 
 import UIKit
 
@@ -7,7 +15,6 @@ import UIKit
 for numeros in 0...100 {
     
     var num = numeros;
-    
     
     switch numeros {
     
@@ -24,7 +31,7 @@ for numeros in 0...100 {
     case num where num % 2 != 0:
         print("\(num)\tImpar")
     default:
-        print("\(num)\tno existe el numero")
+        print("\(num)\tNo existe el numero")
     }
     }
 
